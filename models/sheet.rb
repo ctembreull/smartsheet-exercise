8bc210e33ba532186957a1459c60135c3bbcd85d
Chris Tembreull <chris@tembreull.org>
2015-08-25 12:16:37 -0700

    Convert to full AR model (instead of virtual)

--- models/sheet.rb
+++ models/sheet.rb
@@ -1,16 +1,25 @@
-class Sheet
-  attr_reader :id, :name, :accessLevel, :permalink, :created_at, :modified_at, :columns
-  def initialize(sheet)
-    @id          = sheet['id']
-    @name        = sheet['name']
-    @accessLevel = sheet['accessLevel']
-    @permalink   = sheet['permalink']
-    @created_at  = sheet['createdAt']
-    @modified_at = sheet['modifiedAt']
-  end
+class Sheet < ActiveRecord::Base
+
+  belongs_to :container
+  belongs_to :home
 
   def columns
     @columns = []
     conn = Faraday.new('https://api.smartsheet.com')
   end
+
+  def self.find_or_create_from(sheet, container_id, home_id)
+    Sheet.find_by(smartsheet_id: sheet['id']) || Sheet.create(
+      smartsheet_id: sheet['id'],
+      container_id:  container_id,
+      home_id:       home_id,
+      name:          sheet['name'],
+      access_level:  sheet['accessLevel'],
+      permalink:     sheet['permalink'],
+      created_at:    sheet['createdAt'],
+      modified_at:   sheet['modifiedAt']
+    )
+  end
+
+
 end
