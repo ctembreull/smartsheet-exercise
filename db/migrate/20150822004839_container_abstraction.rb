ec5bfe03654d36c47e52838ee8596392abe61782
Chris Tembreull <chris@tembreull.org>
2015-08-25 12:27:49 -0700

    make sure smartsheet_id is unique

--- db/migrate/20150822004839_container_abstraction.rb
+++ db/migrate/20150822004839_container_abstraction.rb
@@ -4,7 +4,7 @@ class ContainerAbstraction < ActiveRecord::Migration
     create_table :containers do |t|
       t.string  :type
       t.integer :container_id, index: true
-      t.string  :smartsheet_id, null: false, index: true
+      t.string  :smartsheet_id, null: false, index: true, unique: true
       t.string  :name          # null in case of type:home
       t.string  :permalink     # null in case of type:home
       t.string  :access_level  # null in case of type:folder or type:home
