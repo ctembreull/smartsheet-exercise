d33c43da568a91ad904231769d5fc4c674377276
Chris Tembreull <chris@tembreull.org>
2015-08-25 12:30:19 -0700

    delete unwanted stuff

--- db/migrate/20150819223300_create_homes.rb
+++
@@ -1,8 +0,0 @@
-class CreateHomes < ActiveRecord::Migration
-  def change
-    create_table :homes do |t|
-      t.integer :user_id, index: true, required: true
-      t.string  :raw_json
-    end
-  end
-end
